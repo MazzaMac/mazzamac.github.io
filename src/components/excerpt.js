cd2e1abe6f43ede24a104e8f3fb3a23f41179c5f
RHPortfolio <92521047+RHPortfolio@users.noreply.github.com>
2024-08-07 20:02:06 +0100

    Implements Post/Writing categorisation system

--- src/components/excerpt.js
+++ src/components/excerpt.js
@@ -2,26 +2,35 @@ import React from 'react';
 import { Link } from "gatsby";
 import Img from 'gatsby-image';
 
-export default function Excerpt({title, date, text, timeToRead, slug, category, tags, link, image, product, productLink, categoryFlag, redbubble, redbubbleCollection, redbubbleCollectionLink, etsyDescription, etsy}){
+export default function Excerpt({title, date, text, timeToRead, slug, category, tags, link, image, product, productLink, displayCategoryFlag = true, redbubble, redbubbleCollection, redbubbleCollectionLink, etsyDescription, etsy}){
 
-    
     return (
-        <div style={{
-            marginBottom: 50
-        }}>
-             { image && (
-               <Link to={slug}><Img fluid={image} alt={title}/> </Link>
-            )}
-            
-            { text && <p>{text}</p> }
-            { title && <p>{title}</p> }
-            <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'flex-start'}}>
-            <section style={{flex: 2, display: 'flex', flexDirection: "row", width: "100%", justifyContent: "space-between"}}>
-                    { !link && <Link to={slug}><h2>{category}</h2></Link>}
-                    { link && <a href={link}><h2 >{category}</h2></a>}
+        <div style={{ marginBottom: 50 }}>
+            { title && (
+                <h2 class="front-page">{displayCategoryFlag ? (category + ": ") : ""}{title}</h2>) }
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                alignItems: 'flex-start'}}>
+            <section style={{
+                flex: 2,
+                display: 'flex',
+                flexDirection: "row",
+                width: "100%",
+                justifyContent: "space-between"}}>
                    <small><b>{date}</b></small>
                 </section>
             </div>
+
+             { image && (
+               <Link to={slug} ><Img fluid={image} alt={title} /> </Link>
+            )}
+            
+            { text && <div dangerouslySetInnerHTML={{__html: text }}/> }
+            <hr />
+           
+            
             { product && <p>This is available in the <Link to="/shop" >shop</Link>
                 { redbubble && !etsy && " through " + <a href={redbubble}>Redbubble</a>}
                 { !redbubble && etsy && ", through " + <a href={etsy}>Etsy</a>}
